d2dbfecd5d21a9d67c7f7815bc6e5df9d62b66e6
danieldgtal <danielchiatuiro@gmail.com>
2025-10-09 19:20:53 -0400

    building

--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./SingleProductPage.css";
 
@@ -9,32 +9,56 @@ const product = {
 		"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid rerum a? Fugiat soluta facilis deleniti voluptatibus ab architecto dolores a, vero, beatae veniam error doloribus quia laudantium? Error fuga consequuntur quia accusantium? Consequatur modi laboriosam saepe culpa, ab atque.",
 	price: 9.99,
 	images: [
-		"https://via.placeholder.com/500x500?text=Product+Image+1",
-		"https://via.placeholder.com/500x500?text=Product+Image+2",
-		"https://via.placeholder.com/500x500?text=Product+Image+3",
-		"https://via.placeholder.com/500x500?text=Product+Image+4",
+		"https://placehold.co/500x500?text=Product+Image+1",
+		"https://placehold.co/500x500?text=Product+Image+2",
+		"https://placehold.co/500x500?text=Product+Image+3",
+		"https://placehold.co/500x500?text=Product+Image+4",
 	],
 	stock: 10,
 };
 
 const SingleProductPage = () => {
+	const [selectedImage, setSelectedImage] = useState(0);
+
 	return (
 		<section>
 			<div className="align_center single_product">
 				<div className="align_center">
 					<div className="single_product_thumbnails">
 						{product.images.map((image, index) => (
-							<img key={index} src={image} alt={product.title} />
+							<img
+								key={index}
+								src={image}
+								alt={product.title}
+								className={selectedImage === index ? "selected_image" : ""}
+								onClick={() => setSelectedImage(index)}
+							/>
 						))}
 					</div>
 				</div>
 				<img
-					src={product.images[0]}
+					src={product.images[selectedImage]}
 					alt={product.title}
 					className="single_product_display"
 				/>
 			</div>
-			<div className="align_center single_product_details"></div>
+			<div className="single_product_details">
+				<h1 className="single_product_title">{product.title}</h1>
+				<p className="single_product_description">{product.description}</p>
+				<p className="single_product_price">${product.price.toFixed(2)}</p>
+
+				<h2 className="quantity_title">
+					Quantity:
+					<div className="align_center quantity_input">
+						<button className="quantity_input_button" disabled>
+							-
+						</button>
+						<p className="quantity_input_count">1</p>
+						<button className="quantity_input_button"> + </button>
+					</div>
+					<button className="search_button add_cart">Add to Cart</button>
+				</h2>
+			</div>
 		</section>
 	);
 };
