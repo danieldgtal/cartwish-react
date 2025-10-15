ea4377f0ed7079d2d3176b1cbc34f12344a03abf
danieldgtal <danielchiatuiro@gmail.com>
2025-10-15 18:16:35 -0400

    finishing react application and integrating backend

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import "./Navbar.css";
 import rocket from "../../assets/rocket.png";
@@ -10,34 +11,34 @@ import lock from "../../assets/locked.png";
 import LinkWithIcon from "./LinkWithIcon";
 
 const Navbar = () => {
-    return (
-        <nav className='align_center navbar'>
-            <div className='align_center'>
-                <h1 className='navbar_heading'>CartWish</h1>
-                <form className='align_center navbar_form'>
-                    <input
-                        type='text'
-                        className='navbar_search'
-                        placeholder='Search Products'
-                    />
-                    <button type='submit' className='search_button'>
-                        Search
-                    </button>
-                </form>
-            </div>
-            <div className='align_center navbar_links'>
-                <LinkWithIcon title='Home' link='/' emoji={rocket} />
-                <LinkWithIcon title='Products' link='/products' emoji={star} />
-                <LinkWithIcon title='LogIn' link='/login' emoji={idButton} />
-                <LinkWithIcon title='SignUp' link='/signup' emoji={memo} />
-                <LinkWithIcon title='My Orders' link='myorders' emoji={order} />
-                <LinkWithIcon title='Logout' link='/logout' emoji={lock} />
-                <a href='/cart' className='align_center'>
-                    Cart <p className='align_center cart_counts'>0</p>
-                </a>
-            </div>
-        </nav>
-    );
+	return (
+		<nav className="align_center navbar">
+			<div className="align_center">
+				<h1 className="navbar_heading">CartWish</h1>
+				<form className="align_center navbar_form">
+					<input
+						type="text"
+						className="navbar_search"
+						placeholder="Search Products"
+					/>
+					<button type="submit" className="search_button">
+						Search
+					</button>
+				</form>
+			</div>
+			<div className="align_center navbar_links">
+				<LinkWithIcon title="Home" link="/" emoji={rocket} />
+				<LinkWithIcon title="Products" link="/products" emoji={star} />
+				<LinkWithIcon title="LogIn" link="/login" emoji={idButton} />
+				<LinkWithIcon title="SignUp" link="/signup" emoji={memo} />
+				<LinkWithIcon title="My Orders" link="myorders" emoji={order} />
+				<LinkWithIcon title="Logout" link="/logout" emoji={lock} />
+				<NavLink to="/cart" className="align_center">
+					Cart <p className="align_center cart_counts">0</p>
+				</NavLink>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
